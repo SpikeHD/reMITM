dd21862cf527ae6142f8250742d8dcfeec607187
SpikeHD <spikegdofficial@gmail.com>
2023-06-25 15:41:39 -0700

    config stuff

---
+++ src-tauri/src/config.rs
@@ -0,0 +1,57 @@
+use serde::de::value::Error;
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Deserialize, Serialize)]
+pub struct Config {
+  pub launch_at_startup: bool,
+  pub proxy_port: u16,
+  pub urls_to_redirect: Vec<String>,
+  pub redirect_to: String,
+}
+
+pub fn config_path() -> PathBuf {
+  let mut path = dirs::config_dir().unwrap();
+  path.push("reMITM");
+  path.push("config.json");
+  path
+}
+
+#[tauri::command]
+pub fn get_config() -> Config {
+  let path = config_path();
+
+  if path.exists() {
+    let contents = fs::read_to_string(path).unwrap();
+    serde_json::from_str(&contents).unwrap()
+  } else {
+    Config {
+      launch_at_startup: false,
+      proxy_port: 8111,
+      urls_to_redirect: vec![],
+      redirect_to: "http://localhost:3000".to_string(),
+    }
+  }
+}
+
+pub fn init_config() {
+  let path = config_path();
+
+  // Ensure config_dir/reMITM exists
+  if !path.parent().unwrap().exists() {
+    fs::create_dir_all(path.parent().unwrap()).unwrap();
+  }
+
+  if !path.exists() {
+    let config = Config {
+      launch_at_startup: false,
+      proxy_port: 8111,
+      urls_to_redirect: vec![],
+      redirect_to: "http://localhost:3000".to_string(),
+    };
+
+    let config_json = serde_json::to_string(&config).unwrap();
+    fs::write(path, config_json).unwrap();
+  }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use config::init_config;
+
+mod config;
+mod proxy;
+
+/**
+ * Ensures config path and file exists
+ */
+pub fn init() {
+  init_config();
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![
+      config::get_config,
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
